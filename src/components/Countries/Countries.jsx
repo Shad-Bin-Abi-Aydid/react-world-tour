28ace78e6aea58b65f2568f2a01cd623e2bc07f9
shad-bin-abi-aydid <shadaydid@gmail.com>
2025-03-02 17:38:58 +0000

    List of visited country are showing in the top of the page

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,28 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import Country from '../Country/Country';
-import './Countries.css'
+import React, { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
 export default function Countries() {
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
-    const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    useEffect(() =>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+  const handleVisitedCountry = (country) => {
+    
+    // In react, this is the way of push array
+    const newVisitedCountries = [...visitedCountries, country];
+
+    setVisitedCountries(newVisitedCountries);
+
+  };
 
   return (
     <div>
-      Countries:{countries.length} 
-
-      <div className='countries'>
-      {
-        countries.map(country => <Country key={countries.cca3} country = {country}></Country>)
-      }
+      Countries: {countries.length}
+      <div>
+        <h4>Visited Countries: {visitedCountries.length}</h4>
+        <ul>
+            {
+              visitedCountries.map(country => <li>{country.name.common}</li>)
+            }
+        </ul>
       </div>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country 
+          key={countries.cca3} 
+          handleVisitedCountry = {handleVisitedCountry}
+          country={country}>
 
+          </Country>
+        ))}
+      </div>
     </div>
-    
-  )
+  );
 }
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Country.css'
 
-export default function Country({country}) {
+export default function Country({country, handleVisitedCountry}) {
 
   const {name, flags,population, area,cca3} = country;
 
@@ -11,15 +11,17 @@ export default function Country({country}) {
     setVisited(!visited);
   }
 
-  console.log(country);
 
   return (
-    <div className='country'>
+    <div className={` country ${visited? 'visited' : 'nonVisited'}`}>
       <h3>{name.common}</h3>
       <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
       <p>Code: {cca3}</p>
+      <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+      <br />
+      <br />
       <button onClick={handleVisited} >{visited ? 'Visited':'Going'}</button>
       {
         visited ? ' I visited this place':' I want to visit'
